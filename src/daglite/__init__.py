e28d9e69aae3580b0ddd129e3b29aba033779b92
Charles Swartz <charles.w.swartz.vi@gmail.com>
2026-03-03 18:30:27 -0500

    Update public API from top-level subpackages

--- src/daglite/__init__.py
+++ src/daglite/__init__.py
@@ -5,12 +5,12 @@ __version__ = "0.8.0"
 import daglite.datasets.builtin  # noqa: F401  ensure builtins are registered
 from daglite import backends
 from daglite import settings
-from daglite.eager import eager_task as task
 from daglite.mapping import async_task_map
 from daglite.mapping import task_map
 from daglite.plugins.manager import _initialize_plugin_system
 from daglite.session import async_session
 from daglite.session import session
+from daglite.tasks import task
 from daglite.workflows import workflow
 
 # Initialize hooks system on module import
